5bdc628a7b240cf0083bc159a6bcc7236a2b3167
Benoît Hubert <benoithubert@gmail.com>
2016-09-17 03:04:57 +0200

    Copied scaffolded unit test files from a real plugin, modified tests/bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * PHPUnit bootstrap file
+ *
+ * @package Sandbox_Plugin
+ */
+
+$_tests_dir = getenv( 'WP_TESTS_DIR' );
+if ( ! $_tests_dir ) {
+	$_tests_dir = '/tmp/wordpress-tests-lib';
+}
+
+// Give access to tests_add_filter() function.
+require_once $_tests_dir . '/includes/functions.php';
+
+define('SRC_DIR', dirname( dirname( __FILE__ ) ) . '/src/bhubr');
+/**
+ * Manually load the plugin being tested.
+ */
+function _manually_load_plugin() {
+    require SRC_DIR . '/vendor/Inflect.php';
+	require SRC_DIR . '/Base_Model.php';
+    require SRC_DIR . '/Post_Model.php';
+    require SRC_DIR . '/Term_Model.php';
+}
+tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
+
+// Start up the WP testing environment.
+require $_tests_dir . '/includes/bootstrap.php';
---
+++ tests/test_Base_Model.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * Class SampleTest
+ *
+ * @package Sandbox_Plugin
+ */
+
+/**
+ * Sample test case.
+ */
+class Base_Model_Test extends WP_UnitTestCase {
+
+    function setUp() {
+        bhubr\Base_Model::register_type('pouet', 'Pouet', ['fields' => ['foo', 'bar']]);
+        bhubr\Base_Model::register_taxonomy('pouetax', 'Pouetaxonomy', 'pouet', ['baz', 'boo']);
+    }
+
+    /**
+     * A single example test.
+     */
+    function test_registered_types() {
+        // Replace this with some actual testing code.
+        $builtin_types = get_post_types(['_builtin' => true]);
+        $all_types = array_merge($builtin_types, ['pouet' => 'pouet']);
+        $this->assertEquals( $all_types, get_post_types() );
+    }
+
+    /**
+     * A single example test.
+     */
+    function test_registered_taxonomies() {
+        // Replace this with some actual testing code.
+        $builtin_taxonomies = get_taxonomies(['_builtin' => true]);
+        $all_taxos = array_merge($builtin_taxonomies, ['pouetax' => 'pouetax']);
+        $this->assertEquals( $all_taxos, get_taxonomies() );
+    }
+
+}
